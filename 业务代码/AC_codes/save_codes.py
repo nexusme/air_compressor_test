977de55a73bacbcecae38e3e82d7e4f28682673a
Nexus Gong <you@nexus_me@hotmail.com>
2023-01-24 20:59:17 +0800

    update calfucs

--- 业务代码/AC_codes/save_codes.py
+++ 业务代码/AC_codes/save_codes.py
@@ -17,10 +17,13 @@ def complete_data(air_dict):
             row['ser_p'] = service_para_dict[row['brand']]
             row['por'] = por_dict[row['brand']]
         else:
-            row['brand'] = '其他'
             row['ser_p'] = 1.3
             row['por'] = 0.9
     return air_dict
+
+
+def calculate_process(air_dict):
+    return calculate_it_noneFC(air_dict) if not air_dict['isFC'] else calculate_it_FC(air_dict)
 
 
 def read_dict(air_dict):
@@ -34,22 +37,55 @@ def read_dict(air_dict):
     return [no, model, run_time, load_time, ori_power, air, brand, ori_pre, actual_pre, ser_p, d_val, por]
 
 
-def calculate_it(air_dict):
+def calculate_it_noneFC(air_dict):
     dict_read = read_dict(air_dict)
     no, model, run_time, load_time = dict_read[0], dict_read[1], dict_read[2], dict_read[3]
     ori_power, air = dict_read[4], dict_read[5]
     brand, ori_pre, actual_pre = dict_read[6], dict_read[7], dict_read[8]
     ser_p = dict_read[9]
-    por = dict_read[11] if air_dict['isFC'] else float(round(load_time / run_time, 4))
+    # 空载浪费
+    d_val = dict_read[10]
+
+    empty_waste = round((run_time - load_time) / run_time * 0.4 * ori_power, 4)
+    str_empty_waste = '(' + str(run_time) + '-' + str(load_time) + ')' + '/' + str(run_time) + '*40%' + '*' + str(
+        ori_power) + '=' + str(empty_waste)
+
+    # 工频压降浪费
+    d_val_waste = round(ori_power * d_val * 0.07, 4)
+    str_d_val_waste = str(ori_power) + '*' + '(' + str(d_val) + ')*7%=' + str(round(ori_power * d_val * 0.07, 3))
+
+    # 总计浪费
+    total_waste = round(empty_waste + d_val_waste, 4)
+    # print(total_waste)
+
+    por = float(round(load_time / run_time, 4))
+
+    actual_energyE = round((float(ori_power) * float(ser_p) * por + empty_waste + d_val_waste) / (air * por), 2)
+    str_actual_energyE = '(' + str(ori_power) + '*' + str(ser_p) + '*' + str(por) + '+' + str(
+        total_waste) + ')/(' + str(air) + '*' + str(por) + ')=' + str(actual_energyE)
+    print(no, brand, model, run_time, load_time, ori_power, air, d_val, '实加载比例：', str(por), '实用气量：',
+          str(round(por * air, 4)), '实比功率：' + str(actual_energyE))
+    isFccn = '变频 ' if air_dict['isFC'] else '工频'
+    return [[str(no), model, str(run_time), str(load_time), str(ori_power), str_empty_waste, str_d_val_waste,
+             str(total_waste), str_actual_energyE, round(por * air, 4)],
+            [str(ori_power), str(air), str(ori_pre), str(actual_pre), str(model), str(brand)],
+            [brand, model, ori_power, isFccn, actual_energyE, round(actual_energyE / 60, 4)]]
+
+
+def calculate_it_FC(air_dict):
+    dict_read = read_dict(air_dict)
+    no, model, run_time, load_time = dict_read[0], dict_read[1], dict_read[2], dict_read[3]
+    ori_power, air = dict_read[4], dict_read[5]
+    brand, ori_pre, actual_pre = dict_read[6], dict_read[7], dict_read[8]
+    ser_p = dict_read[9]
+    por = dict_read[11]
 
     # 空载浪费
     d_val = dict_read[10]
 
     # 空载浪费
-    empty_waste = 0 if air_dict['isFC'] else round((run_time - load_time) / run_time * 0.4 * ori_power, 4)
-    str_empty_waste = '0' if air_dict['isFC'] else '(' + str(run_time) + '-' + str(load_time) + ')' + '/' + str(
-        run_time) + '*40%' + '*' + str(ori_power) + '=' + str(empty_waste)
-
+    empty_waste = 0
+    str_empty_waste = '0'
     # 压降浪费
     d_val_waste = round(ori_power * d_val * 0.07, 4)
     d_cal_wast_por = 1 - round(d_val * 0.07, 4)
@@ -57,14 +93,10 @@ def calculate_it(air_dict):
 
     # 总计浪费
     total_waste = round(empty_waste + d_val_waste, 4)
-
-    actual_energyE = round((float(ori_power) * ser_p * d_cal_wast_por) / (air * por), 2) if air_dict['isFC'] else round(
-        (float(ori_power) * float(ser_p) * por + empty_waste + d_val_waste) / (air * por), 2)
+    # print(total_waste)
+    actual_energyE = round((float(ori_power) * ser_p * d_cal_wast_por) / (air * por), 2)
     str_actual_energyE = '(' + str(ori_power) + '*' + str(ser_p) + '*' + str(d_cal_wast_por) + ')/(' + str(
-        air) + '*' + str(por) + ')=' + str(actual_energyE) if air_dict['isFC'] else '(' + str(ori_power) + '*' + str(
-        ser_p) + '*' + str(por) + '+' + str(
-        total_waste) + ')/(' + str(air) + '*' + str(por) + ')=' + str(actual_energyE)
-
+        air) + '*' + str(por) + ')=' + str(actual_energyE)
     print(no, brand, model, run_time, load_time, ori_power, air, d_val, '实加载比例：', str(por), '实用气量：',
           str(round(por * air, 4)), '实比功率：' + str(actual_energyE))
     isFccn = '变频 ' if air_dict['isFC'] else '工频'
@@ -83,7 +115,7 @@ def originEC_to_dataframe(machine1, new_machine):
     all_year_savings = 0
     ee_dicts = []
     for one in machine1:
-        data_list, eq_list, ee_list = calculate_it(one)
+        data_list, eq_list, ee_list = calculate_process(one)
         tmp_dict = {'brand': ee_list[0],
                     'model': ee_list[1],
                     'ori_power': ee_list[2],
